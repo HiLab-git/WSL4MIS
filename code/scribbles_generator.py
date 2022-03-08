499cb2410ac2e68d65f6725cdc4a27219afedafd
Xiangde Luo <19658103+Luoxd1996@users.noreply.github.com>
2022-03-08 15:57:13 +0800

    Update scribbles_generator.py

--- code/scribbles_generator.py
+++ code/scribbles_generator.py
@@ -7,8 +7,14 @@
 #   title={{WORD}: Revisiting Organs Segmentation in the Whole Abdominal Region},
 #   author={Luo, Xiangde and Liao, Wenjun and Xiao, Jianghong and Song, Tao and Zhang, Xiaofan and Li, Kang and Wang, Guotai and Zhang, Shaoting},
 #   journal={arXiv preprint arXiv:2111.02403},
-#   year={2021}
-# }
+#   year={2021}}
+#
+#   @article{luo2022scribbleseg,
+#   title={Scribble-Supervised Medical Image Segmentation via Dual-Branch Network and Dynamically Mixed Pseudo Labels Supervision},
+#   author={Xiangde Luo, Minhao Hu, Wenjun Liao, Shuwei Zhai, Tao Song, Guotai Wang, Shaoting Zhang},
+#   journal={arXiv preprint arXiv:2203.02106},
+#   year={2022}}
+
 
 import glob
 import math
@@ -23,7 +29,7 @@ from scipy import ndimage
 from skimage.morphology import skeletonize
 
 sys.setrecursionlimit(1000000)
-seed = 2021
+seed = 2022
 np.random.seed(seed)
 random.seed(seed)
 
