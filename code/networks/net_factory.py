9ec181283652b686f057d0006981528fb2c86807
tianbiao <648283812@qq.com>
2023-03-04 15:20:31 +0800

    modify code

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -1,11 +1,11 @@
 from networks.efficientunet import Effi_UNet
-from networks.enet import ENet
+# from networks.enet import ENet
 from networks.pnet import PNet2D
 from networks.unet import UNet, UNet_DS, UNet_URPC, UNet_CCT
 import argparse
 from networks.vision_transformer import SwinUnet as ViT_seg
 from networks.config import get_config
-from networks.nnunet import initialize_network
+# from networks.nnunet import initialize_network
 
 
 parser = argparse.ArgumentParser()
@@ -75,8 +75,8 @@ config = get_config(args)
 def net_factory(net_type="unet", in_chns=1, class_num=3):
     if net_type == "unet":
         net = UNet(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "enet":
-        net = ENet(in_channels=in_chns, num_classes=class_num).cuda()
+    # elif net_type == "enet":
+    #     net = ENet(in_channels=in_chns, num_classes=class_num).cuda()
     elif net_type == "unet_ds":
         net = UNet_DS(in_chns=in_chns, class_num=class_num).cuda()
     elif net_type == "unet_cct":
@@ -91,8 +91,8 @@ def net_factory(net_type="unet", in_chns=1, class_num=3):
                       num_classes=args.num_classes).cuda()
     elif net_type == "pnet":
         net = PNet2D(in_chns, class_num, 64, [1, 2, 4, 8, 16]).cuda()
-    elif net_type == "nnUNet":
-        net = initialize_network(num_classes=class_num).cuda()
+    # elif net_type == "nnUNet":
+    #     net = initialize_network(num_classes=class_num).cuda()
     else:
         net = None
     return net
