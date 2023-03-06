80bf1b25dadf858eaabf553c9f52df5b8d2d5d01
tianbiao <648283812@qq.com>
2023-03-06 09:38:13 +0800

    modify  code

--- code/train_weakly_supervised_pCE_2D.py
+++ code/train_weakly_supervised_pCE_2D.py
@@ -28,7 +28,7 @@ from val_2D import test_single_volume, test_single_volume_ds
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_path', type=str,
-                    default='../data/ACDC', help='Name of Experiment')
+                    default='/mnt/sdd/yd2tb/data/ACDC', help='Name of Experiment')
 parser.add_argument('--exp', type=str,
                     default='ACDC_pCE', help='experiment_name')
 parser.add_argument('--fold', type=str,
@@ -41,7 +41,7 @@ parser.add_argument('--num_classes', type=int,  default=4,
                     help='output channel of network')
 parser.add_argument('--max_iterations', type=int,
                     default=30000, help='maximum epoch number to train')
-parser.add_argument('--batch_size', type=int, default=24,
+parser.add_argument('--batch_size', type=int, default=48,
                     help='batch_size per gpu')
 parser.add_argument('--deterministic', type=int,  default=1,
                     help='whether use deterministic training')
@@ -51,6 +51,12 @@ parser.add_argument('--patch_size', type=list,  default=[256, 256],
                     help='patch size of network input')
 parser.add_argument('--seed', type=int,  default=2022, help='random seed')
 args = parser.parse_args()
+
+"""选择GPU ID"""
+gpu_list = [4] #[0,1]
+gpu_list_str = ','.join(map(str, gpu_list))
+os.environ.setdefault("CUDA_VISIBLE_DEVICES", gpu_list_str)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def train(args, snapshot_path):
@@ -189,7 +195,7 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
 
-    snapshot_path = "../model/{}_{}/{}".format(args.exp, args.fold, args.sup_type)
+    snapshot_path = "/mnt/sdd/yd2tb/work_dirs/model/{}_{}/{}".format(args.exp, args.fold, args.sup_type)
     if not os.path.exists(snapshot_path):
         os.makedirs(snapshot_path)
 
