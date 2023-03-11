75291dc3428af292855c8cac76811c3ab2be723c
tianbiao <648283812@qq.com>
2023-03-11 09:30:15 +0800

    modify code

--- code/train_Trans_teacher.py
+++ code/train_Trans_teacher.py
@@ -31,13 +31,13 @@ from val_2D import test_single_volume
 from networks.vision_transformer import SwinUnet as ViT_seg
 from config import get_config
 from torch.nn import CosineSimilarity
-
+from torch.utils.data.distributed import DistributedSampler
 # """选择GPU ID"""
 # gpu_list = [4] #[0,1]
 # gpu_list_str = ','.join(map(str, gpu_list))
 # os.environ.setdefault("CUDA_VISIBLE_DEVICES", gpu_list_str)
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+from utils.gate_crf_loss import ModelLossSemsegGatedCRF
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_path', type=str,
@@ -52,7 +52,7 @@ parser.add_argument('--sup_type', type=str,
                     default='scribble', help='supervision type')
 parser.add_argument('--max_iterations', type=int,
                     default=30000, help='maximum epoch number to train')
-parser.add_argument('--batch_size', type=int, default=16,
+parser.add_argument('--batch_size', type=int, default=8,
                     help='batch_size per gpu')
 parser.add_argument('--deterministic', type=int,  default=1,
                     help='whether use deterministic training')
@@ -65,7 +65,7 @@ parser.add_argument('--num_classes', type=int,  default=4,
                     help='output channel of network')
 
 # label and unlabel
-parser.add_argument('--labeled_bs', type=int, default=8,
+parser.add_argument('--labeled_bs', type=int, default=4,
                     help='labeled_batch_size per gpu')
 parser.add_argument('--labeled_num', type=int, default=4,
                     help='labeled data')
@@ -109,10 +109,12 @@ parser.add_argument('--throughput', action='store_true',
 parser.add_argument('--my_lambda', type=float,  default=1, help='balance factor to control contrastive loss')
 parser.add_argument('--tau', type=float,  default=1, help='temperature of the contrastive loss')
 
+parser.add_argument("--local_rank", default=os.getenv('LOCAL_RANK', 2), type=int)
+
 args = parser.parse_args()
 config = get_config(args)
-
-device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
+# 
+device = torch.device('cuda:4' if torch.cuda.is_available() else 'cpu')
 
 def get_current_consistency_weight(epoch):
     # Consistency ramp-up from https://arxiv.org/abs/1610.02242
@@ -127,6 +129,12 @@ def update_ema_variables(model, ema_model, alpha, global_step):
 
 
 def train(args, snapshot_path):
+
+    # if args.local_rank != -1:
+    #         torch.cuda.set_device(args.local_rank)
+    #         device=torch.device("cuda", args.local_rank)
+    #         torch.distributed.init_process_group(backend="nccl", init_method='env://') 
+
     base_lr = args.base_lr
     num_classes = args.num_classes
     batch_size = args.batch_size
@@ -137,24 +145,47 @@ def train(args, snapshot_path):
 
     def create_model(ema=False):
         # Network definition
-        # model = net_factory(net_type=args.model, in_chns=1,class_num=num_classes)
-        model = ViT_seg(config, img_size=args.patch_size,num_classes=args.num_classes)
+        model = net_factory(net_type=args.model, in_chns=1,class_num=num_classes)
+       
         if ema:
             for param in model.parameters():
                 param.detach_()
         return model
     
     model = create_model()
-    ema_model = create_model(ema=True)
+    # ema_model = create_model(ema=True)
+    model2 = ViT_seg(config, img_size=args.patch_size,num_classes=args.num_classes)
+    model=model.to(device)
+    model2=model2.to(device)
 
-    model=model.to(device) 
-    ema_model=ema_model.to(device) 
+    # ema_model=ema_model.to(device) 
+    # model = nn.MMDistributedDataParallel(
+    #     model.cuda(),
+    #     device_ids=[torch.cuda.current_device()],
+    #     broadcast_buffers=False,
+    #     find_unused_parameters=find_unused_parameters)
 
+
+    num_gpus = torch.cuda.device_count()
+
+    # if num_gpus > 1:
+    #     # logger.info('use {} gpus!'.format(num_gpus))
+    #     model = nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
+    #                                             output_device=args.local_rank,broadcast_buffers=False)    
     db_train_labeled = BaseDataSets(base_dir=args.root_path, num=8, labeled_type="labeled", fold=args.fold, split="train", transform=transforms.Compose([
-        RandomGenerator(args.patch_size)
-    ]))
+        RandomGenerator(args.patch_size)]),sup_type=args.sup_type)
     db_train_unlabeled = BaseDataSets(base_dir=args.root_path, num=8, labeled_type="unlabeled", fold=args.fold, split="train", transform=transforms.Compose([
         RandomGenerator(args.patch_size)]))
+
+    #步骤四：定义数据集
+    # train_datasets = ...#自己定义的Dataset子类
+    # train_sampler_labeled = DistributedSampler(db_train_labeled)
+    # train_sampler_unlabeled = DistributedSampler(db_train_unlabeled)
+
+    # trainloader_labeled = DataLoader(db_train_labeled, sampler=train_sampler_labeled, batch_size=args.train_batch_size,
+                                #   num_workers=args.num_workers, drop_last=True,pin_memory=True)
+    # trainloader_unlabeled = DataLoader(db_train_unlabeled, sampler=train_sampler_unlabeled, batch_size=args.train_batch_size,
+                                #   num_workers=args.num_workers, drop_last=True,pin_memory=True)
 
     trainloader_labeled = DataLoader(db_train_labeled, batch_size=args.batch_size//2, shuffle=True,
                                      num_workers=16, pin_memory=True, drop_last=True,worker_init_fn=worker_init_fn)
@@ -174,6 +205,10 @@ def train(args, snapshot_path):
     ce_loss = CrossEntropyLoss(ignore_index=4)
     dice_loss = losses.pDLoss(num_classes, ignore_index=4)
     cos_sim = CosineSimilarity(dim=1,eps=1e-6)
+    gatecrf_loss = ModelLossSemsegGatedCRF()
+    loss_gatedcrf_kernels_desc = [{"weight": 1, "xy": 6, "rgb": 0.1}]
+    loss_gatedcrf_radius = 5
+
 
     writer = SummaryWriter(snapshot_path + '/log')
     logging.info("{} iterations per epoch".format(len(trainloader_labeled)))
@@ -183,6 +218,7 @@ def train(args, snapshot_path):
     best_performance = 0.0
     iterator = tqdm(range(max_epoch), ncols=70)
     for epoch_num in iterator:
+        # train_sampler_labeled.set_epoch(epoch_num)
         for i, data in enumerate(zip(cycle(trainloader_labeled), trainloader_unlabeled)):
             sampled_batch_labeled, sampled_batch_unlabeled = data[0], data[1]
 
@@ -191,86 +227,95 @@ def train(args, snapshot_path):
             unlabeled_volume_batch = sampled_batch_unlabeled['image'].to(device)
             # print("Labeled slices: ", sampled_batch_labeled["idx"])
             # print("Unlabeled slices: ", sampled_batch_unlabeled["idx"])
+            with torch.autograd.set_detect_anomaly(True):
+                noise = torch.clamp(torch.randn_like(
+                    unlabeled_volume_batch) * 0.1, -0.2, 0.2)
+                ema_inputs = unlabeled_volume_batch + noise
 
-            noise = torch.clamp(torch.randn_like(
-                unlabeled_volume_batch) * 0.1, -0.2, 0.2)
-            ema_inputs = unlabeled_volume_batch + noise
+                volume_batch=torch.cat([volume_batch,unlabeled_volume_batch],0)
 
-            volume_batch=torch.cat([volume_batch,unlabeled_volume_batch],0)
+                outputs,logits,logits_ = model(volume_batch)
+                outputs_soft = torch.softmax(outputs, dim=1)
 
-            outputs,logits,logits_ = model(volume_batch)
-            outputs_soft = torch.softmax(outputs, dim=1)
+                outputs_unlabeled,_,_ = model(volume_batch[args.labeled_bs:,...])
+                outputs_unlabeled_soft = torch.softmax(outputs_unlabeled, dim=1)
 
-            outputs_unlabeled,_,_ = model(volume_batch[args.labeled_bs:,...])
-            outputs_unlabeled_soft = torch.softmax(outputs_unlabeled, dim=1)
-
-            with torch.no_grad():
-                ema_output,ema_logits,ema_logits_  = ema_model(ema_inputs)
-                ema_output_soft = torch.softmax(ema_output, dim=1)
-
-            loss_ce = ce_loss(outputs[:args.labeled_bs,...], label_batch[:].long())
-            loss_dice =ce_loss(outputs[:args.labeled_bs,...], label_batch[:].long()) #dice_loss(outputs_soft, label_batch.unsqueeze(1))
-            # supervised_loss = 0.5 * (loss_dice + loss_ce)
-            supervised_loss=loss_ce
-            consistency_weight = get_current_consistency_weight(iter_num // 300)
-            # if iter_num < 1000:
-            #     consistency_loss = 0.0
-            # else:
-            consistency_loss = torch.mean((outputs_unlabeled_soft - ema_output_soft) ** 2)
-
-
-            create_center_1_bg = logits[0].unsqueeze(1)# 4,1,x,y,z->4,2
-            create_center_1_a =  logits[1].unsqueeze(1)
-            create_center_1_b =  logits[2].unsqueeze(1)
-            create_center_1_c =  logits[3].unsqueeze(1)
+                with torch.no_grad():
+                    ema_output,ema_logits,ema_logits_  = ema_model(ema_inputs)
+                    ema_output_soft = torch.softmax(ema_output, dim=1)
+                out_gatedcrf = gatecrf_loss(
+                    outputs_soft,
+                    loss_gatedcrf_kernels_desc,
+                    loss_gatedcrf_radius,
+                    volume_batch,
+                    256,
+                    256,
+                )["loss"]
+                loss_ce = ce_loss(outputs[:args.labeled_bs,...], label_batch[:].long())
+                loss_dice =ce_loss(outputs[:args.labeled_bs,...], label_batch[:].long())#dice_loss(outputs_soft[:args.labeled_bs,...], label_batch.unsqueeze(1))
+                # supervised_loss = 0.5 * (loss_dice + loss_ce)
+                supervised_loss=loss_ce
+                consistency_weight = get_current_consistency_weight(iter_num // 300)
+                # if iter_num < 1000:
+                #     consistency_loss = 0.0
+                # else:
+                consistency_loss = torch.mean((outputs_unlabeled_soft - ema_output_soft) ** 2)
 
 
+                create_center_1_bg = logits[0].unsqueeze(1)# 4,1,x,y,z->4,2
+                create_center_1_a =  logits[1].unsqueeze(1)
+                create_center_1_b =  logits[2].unsqueeze(1)
+                create_center_1_c =  logits[3].unsqueeze(1)
 
-            create_center_2_bg = logits_[0].unsqueeze(1)
-            create_center_2_a =  logits_[1].unsqueeze(1)
-            create_center_2_b =  logits_[2].unsqueeze(1)
-            create_center_2_c =  logits_[3].unsqueeze(1)
+
+
+                create_center_2_bg = logits_[0].unsqueeze(1)
+                create_center_2_a =  logits_[1].unsqueeze(1)
+                create_center_2_b =  logits_[2].unsqueeze(1)
+                create_center_2_c =  logits_[3].unsqueeze(1)
+                
+                create_center_soft_1_bg = F.softmax(create_center_1_bg, dim=1)# dims(4,2)
+                create_center_soft_1_a = F.softmax(create_center_1_a, dim=1)
+                create_center_soft_1_b = F.softmax(create_center_1_b, dim=1)
+                create_center_soft_1_c = F.softmax(create_center_1_c, dim=1)
+
+
+                create_center_soft_2_bg = F.softmax(create_center_2_bg, dim=1)# dims(4,2)
+                create_center_soft_2_a = F.softmax(create_center_2_a, dim=1)
+                create_center_soft_2_b = F.softmax(create_center_2_b, dim=1)            
+                create_center_soft_2_c = F.softmax(create_center_2_c, dim=1)
+
+
+                lb_center_12_bg = torch.cat((create_center_soft_1_bg[:args.labeled_bs,...], create_center_soft_2_bg[:args.labeled_bs,...]),dim=0)# 4,2
+                lb_center_12_a = torch.cat((create_center_soft_1_a[:args.labeled_bs,...], create_center_soft_2_a[:args.labeled_bs,...]),dim=0)
+                lb_center_12_b = torch.cat((create_center_soft_1_b[:args.labeled_bs,...], create_center_soft_2_b[:args.labeled_bs,...]),dim=0)            
+                lb_center_12_c = torch.cat((create_center_soft_1_c[:args.labeled_bs,...], create_center_soft_2_c[:args.labeled_bs,...]),dim=0)
+                
+
+                un_center_12_bg = torch.cat((create_center_soft_1_bg[args.labeled_bs:,...], create_center_soft_2_bg[args.labeled_bs:,...]),dim=0)
+                un_center_12_a = torch.cat((create_center_soft_1_a[args.labeled_bs:,...], create_center_soft_2_a[args.labeled_bs:,...]),dim=0)
+                un_center_12_b = torch.cat((create_center_soft_1_b[args.labeled_bs:,...], create_center_soft_2_b[args.labeled_bs:,...]),dim=0)        
+                un_center_12_c = torch.cat((create_center_soft_1_c[args.labeled_bs:,...], create_center_soft_2_c[args.labeled_bs:,...]),dim=0)        
             
-            create_center_soft_1_bg = F.softmax(create_center_1_bg, dim=1)# dims(4,2)
-            create_center_soft_1_a = F.softmax(create_center_1_a, dim=1)
-            create_center_soft_1_b = F.softmax(create_center_1_b, dim=1)
-            create_center_soft_1_c = F.softmax(create_center_1_c, dim=1)
+                # cosine similarity
+                loss_contrast = losses.scc_loss(cos_sim, args.tau, lb_center_12_bg,
+                                                lb_center_12_a,un_center_12_bg, un_center_12_a,
+                                                lb_center_12_b,lb_center_12_c,un_center_12_b,un_center_12_c)
+
+                if args.consistency!=0:
+                    consistency_weight = get_current_consistency_weight(iter_num//150)
+                    loss = supervised_loss + consistency_weight*loss_contrast+ consistency_weight *consistency_loss+out_gatedcrf*0.1
+                else:
+                    loss = supervised_loss + args.my_lambda * loss_contrast+ args.my_lambda * consistency_loss+out_gatedcrf*0.1
 
 
-            create_center_soft_2_bg = F.softmax(create_center_2_bg, dim=1)# dims(4,2)
-            create_center_soft_2_a = F.softmax(create_center_2_a, dim=1)
-            create_center_soft_2_b = F.softmax(create_center_2_b, dim=1)            
-            create_center_soft_2_c = F.softmax(create_center_2_c, dim=1)
+                # loss = supervised_loss
+                optimizer.zero_grad()
 
-
-            lb_center_12_bg = torch.cat((create_center_soft_1_bg[:args.labeled_bs,...], create_center_soft_2_bg[:args.labeled_bs,...]),dim=0)# 4,2
-            lb_center_12_a = torch.cat((create_center_soft_1_a[:args.labeled_bs,...], create_center_soft_2_a[:args.labeled_bs,...]),dim=0)
-            lb_center_12_b = torch.cat((create_center_soft_1_b[:args.labeled_bs,...], create_center_soft_2_b[:args.labeled_bs,...]),dim=0)            
-            lb_center_12_c = torch.cat((create_center_soft_1_c[:args.labeled_bs,...], create_center_soft_2_c[:args.labeled_bs,...]),dim=0)
-            
-
-            un_center_12_bg = torch.cat((create_center_soft_1_bg[args.labeled_bs:,...], create_center_soft_2_bg[args.labeled_bs:,...]),dim=0)
-            un_center_12_a = torch.cat((create_center_soft_1_a[args.labeled_bs:,...], create_center_soft_2_a[args.labeled_bs:,...]),dim=0)
-            un_center_12_b = torch.cat((create_center_soft_1_b[args.labeled_bs:,...], create_center_soft_2_b[args.labeled_bs:,...]),dim=0)        
-            un_center_12_c = torch.cat((create_center_soft_1_c[args.labeled_bs:,...], create_center_soft_2_c[args.labeled_bs:,...]),dim=0)        
-        
-            # cosine similarity
-            loss_contrast = losses.scc_loss(cos_sim, args.tau, lb_center_12_bg,
-                                            lb_center_12_a,un_center_12_bg, un_center_12_a,
-                                            lb_center_12_b,lb_center_12_c,un_center_12_b,un_center_12_c)
-
-            if args.consistency!=0:
-                consistency_weight = get_current_consistency_weight(iter_num//150)
-                loss = supervised_loss + consistency_weight*loss_contrast+ consistency_weight *consistency_loss
-            else:
-                loss = supervised_loss + args.my_lambda * loss_contrast+ args.my_lambda * consistency_loss
-
-
-            # loss = supervised_loss + consistency_weight * consistency_loss
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            update_ema_variables(model, ema_model, args.ema_decay, iter_num)
+                # loss.backward(retain_graph=True)
+                loss.backward()
+                optimizer.step()
+                update_ema_variables(model, ema_model, args.ema_decay, iter_num)
 
             lr_ = base_lr * (1.0 - iter_num / max_iterations) ** 0.9
             for param_group in optimizer.param_groups:
